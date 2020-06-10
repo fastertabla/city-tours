4668b46484a60c29ff5274386ead511e8378d0e8
Ganesh <kumar.ganesh@ymail.com>
2020-06-10 16:13:51 +0100

    final commit2

---
+++ src/components/TourList/index.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Tour from "../Tour";
+import "./tourlist.scss";
+import { tourData } from "./../../tourData";
+
+class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+
+  removeTour = (id) => {
+    const { tours } = this.state;
+    const filteredTours = tours.filter((tour) => tour.id !== id);
+    this.setState({ tours: filteredTours });
+  };
+
+  render() {
+    const { tours } = this.state;
+    return (
+      <section className="tourList">
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default TourList;
